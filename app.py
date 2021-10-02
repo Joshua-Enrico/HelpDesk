1d7be02fdb66922169952a270b43d9449a24abc1
yoyogold-a11 <tmrfack@gmail.com>
2021-10-02 09:20:26 -0500

    cleaning

--- app.py
+++ app.py
@@ -10,63 +10,11 @@ import os
 from itsdangerous import URLSafeTimedSerializer
 import pymysql
 import smtplib
-
-
-app = Flask(__name__)
-Bootstrap(app)
-SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Helpdesk:Helpdesk@localhost/User'#create database and user
-s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
-db = SQLAlchemy(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-
-
-
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), unique=True)
-    nombre = db.Column(db.String(30))
-    apellido = db.Column(db.String(30))
-    email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(300))
-    admin = db.Column(db.String(20))
-    confirmed_mail = db.Column(db.String(10))
-    user_id = db.Column(db.String(15), unique=True)
-
-class User_id(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String(15), unique=True)
-    used = db.Column(db.String(15))
-    admin = db.Column(db.String(15))
+from .Class import *
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-class LoginForm(FlaskForm):
-    username = StringField('Usuario', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('Contraseña', validators=[InputRequired(), Length(min=8, max=80)])
-    remember = BooleanField('Recordar sesion')
-
-class RegisterForm(FlaskForm):
-    email = StringField('Correo', validators=[InputRequired(), Email(message='Correo Invalido'), Length(max=50)])
-    username = StringField('Nombre de Usuario', validators=[InputRequired(), Length(min=4, max=15)])
-    nombre = StringField('Nombre', validators=[InputRequired(), Length( max=15)])
-    apellido = StringField('Apellido', validators=[InputRequired(), Length( max=15)])
-    user_id = StringField('Id personal', validators=[InputRequired(), Length(min=3, max=3)])
-    password = PasswordField('Contraseña', validators=[InputRequired(), Length(min=6, max=80)])
-
-class CreateID(FlaskForm):
-    user_id = StringField('ID de usuario', validators=[InputRequired(), Length(min=3, max=3)])
-    admin = StringField('Admin?', validators=[InputRequired(), Length(min=3, max=3)])
-
-class recover(FlaskForm):
-    email = StringField('Correo', validators=[InputRequired(), Email(message='Correo Invalido'), Length(max=50)])
-    user_id = StringField('ID de usuario', validators=[InputRequired(), Length(min=3, max=3)])
-    new_password = PasswordField('Contraseña', validators=[InputRequired(), Length(min=6, max=80)])
 
 @app.route('/')
 def index():
