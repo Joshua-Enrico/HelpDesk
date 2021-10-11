5af9032e046449691aa3d48cc71f928d3a38a2f7
Renzo Perez <renzin@gmail.com>
2021-10-11 12:47:42 -0500

    [ADD] bulma templates use renzo.py to test

---
+++ bulmatemplates/renzo.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/index")
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+
+@app.route("/registra_usuario")
+def user_registre():
+    return render_template('registra_usuario.html')
+
+
+@app.route("/registra_ticket")
+def ticket_registre():
+    return render_template('registra_ticket.html')
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('dashboard.html')
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
