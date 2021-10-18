b4249a626e159e8178cbef911fbf5b5928e54661
Renzo Perez <renzin@gmail.com>
2021-10-18 11:01:29 -0500

    [UPDATE] hide button functionality

--- web_flask/functions/static/js/hidde_button.js
+++ web_flask/functions/static/js/hidde_button.js
@@ -1,10 +1,20 @@
-console.log('hey')
-var hidden = true;
-function action() {
+var hidden = false;
+const thisButton = document.querySelector('.btn-hide');
+const thisContent = document.querySelector('.card-content');
+const iconUp = document.querySelector('.fa-angle-up');
+const iconDown = document.querySelector('.fa-angle-down');
+
+thisButton.addEventListener('click', () =>  {
     hidden = !hidden;
     if(hidden) {
-        document.getElementById('togglee').style.visibility = 'hidden';
+        thisContent.classList.add('is-hidden');
+        iconUp.classList.add('is-hidden');
+        iconDown.classList.remove('is-hidden');
+        console.log(hidden);
     } else {
-        document.getElementById('togglee').style.visibility = 'visible';
+        thisContent.classList.remove('is-hidden');
+        iconUp.classList.remove('is-hidden');
+        iconDown.classList.add('is-hidden');
+        console.log(hidden)
     }
-}
+});
