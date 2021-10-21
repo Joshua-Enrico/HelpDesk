ef3a1aa1eb7b8add3d35f00dea9780fe4d36a21d
rennleon <2829@holbertonschool.com>
2021-10-21 15:05:19 -0500

    Refactor files structure

--- api/v1/endpoints/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" Blueprint for API """
-from flask import Blueprint
-app_views = Blueprint('app_views', __name__, url_prefix='/api/endpoints')
-
-from api.v1.endpoints.users import *
-from api.v1.endpoints.tickets import *
-from api.v1.endpoints.user_summary import *
-from api.v1.endpoints.user_profile_admin import *
