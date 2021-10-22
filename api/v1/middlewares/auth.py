fc7654bb8bfb52e03c6b1280f6ca0d1c23e372ce
rennleon <2829@holbertonschool.com>
2021-10-21 22:57:15 -0500

    Create folder for middlewares

---
+++ api/v1/middlewares/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This module defines middleware functions to authorize API queries  """
+from werkzeug.wrappers import Request, Response, ResponseStream
+from flask import request
+from api.utils import decode_token
+
+
+class AuthMiddleware(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        request = Request(environ)
+
+        authorization = request.headers.get('Authorization', 'Bearer')
+        token = authorization.split()[-1]
+        user = decode_token(token)
+        if user is None and request.method != 'OPTIONS':
+            res = Response('Authorization failed', mimetype= 'text/plain', status=401)
+            return res(environ, start_response)
+
+        environ['user'] = user
+        return self.app(environ, start_response)
