895a9b8853ca8585fa6e69d201bb54cbc5e2b095
rennleon <2829@holbertonschool.com>
2021-10-21 22:52:07 -0500

    Update api

--- api/v1/api.py
+++ api/v1/api.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 """ api aplication """
-from .endpoints import app_views
+from api.v1 import app_views
+from api.v1.middlewares.auth import AuthMiddleware
 from web_flask.models import db
 from os import environ
 from flask import Flask, render_template, make_response, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Helpdesk:Helpdesk@localhost/HelpDesk'#create database and user
 app.register_blueprint(app_views)
+app.wsgi_app = AuthMiddleware(app.wsgi_app)
+
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 
@@ -26,8 +32,6 @@ def not_found(error):
 
 if __name__ == "__main__":
     """ Main Function """
-   #create table
-#    db.create_all()
     db.init_app(app)
     port = 5001
     app.run(host='0.0.0.0', port=port, threaded=True, debug=True)
