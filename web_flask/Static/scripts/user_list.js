ed6b235b6abc30cdbe40c94dbcf09a04cc4face4
rennleon <2829@holbertonschool.com>
2021-10-22 10:31:43 -0500

    Add headers to pass token on request

--- web_flask/Static/scripts/user_list.js
+++ web_flask/Static/scripts/user_list.js
@@ -1,40 +1,40 @@
 
 
 $(document).ready(function () {
-    $.ajax({
-        type: 'GET',
-        data: JSON.stringify({}),
-        url: 'http://localhost:5001/api/v1/users',
-        headers: {'Authorization': 'Bearer ' + $('#token').val()},
-        contentType: 'application/json',
-        success: data => {
-            for (const user of data) {
-                const template = `<tr>
-                <th><input type="checkbox"></th>
-                <th>001</th>
-                <td>
-                  <div class="media-left">
-                    <figure class="image is-64x64">
-                      <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
-                    </figure>
-                  </div>
-                </td>
-                <td></td>
-                <td>IT</td>
-                <td>Administrador</td>
-                <td>Activo</td>
-                <td>
-                  <a class="button">
-                    <span class="icon is-small">
-                      <i class="fa fa-ellipsis-v "></i>
-                    </span>
-                  </a>
-                </td>
-              </tr>`;
-                $('tbody.User-info').append(template);
-            }
-        },
-    });
+  // $.ajax({
+  //     type: 'GET',
+  //     data: JSON.stringify({}),
+  //     url: 'http://localhost:5001/api/v1/users',
+  //     headers: {'Authorization': 'Bearer ' + $('#token').val()},
+  //     contentType: 'application/json',
+  //     success: data => {
+  //         for (const user of data) {
+  //             const template = `<tr>
+  //             <th><input type="checkbox"></th>
+  //             <th>001</th>
+  //             <td>
+  //               <div class="media-left">
+  //                 <figure class="image is-64x64">
+  //                   <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+  //                 </figure>
+  //               </div>
+  //             </td>
+  //             <td></td>
+  //             <td>IT</td>
+  //             <td>Administrador</td>
+  //             <td>Activo</td>
+  //             <td>
+  //               <a class="button">
+  //                 <span class="icon is-small">
+  //                   <i class="fa fa-ellipsis-v "></i>
+  //                 </span>
+  //               </a>
+  //             </td>
+  //           </tr>`;
+  //             $('tbody.User-info').append(template);
+  //         }
+  //     },
+  // });
 
     $("div#wrapper").Grid({
 
@@ -61,6 +61,7 @@ $(document).ready(function () {
         resizable: true,
         server: {
             url: 'http://localhost:5001/api/v1/users',
+            headers: {'Authorization': 'Bearer ' + $('#token').val()},
             then: data => data.map(user =>
                 [gridjs.html(`<input data-id=${ user.id } type="checkbox"></input>`), user.Nombre + ' ' + user.Apellido, user.Area, user.Rol, user.Estado,
 
@@ -92,8 +93,6 @@ $(document).ready(function () {
         },
         className: {
             table: 'table is-striped'
-
         }
     });
-
 });
