0f8f2a65e2840e04009a26f44549a1bcf5cf98da
rennleon <2829@holbertonschool.com>
2021-10-24 18:00:00 -0500

    Update ticket detalle view and controller

---
+++ web_flask/Routes/Agent/ticket_details.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from ...models import app
+from flask_login import login_required
+from flask import render_template, session, redirect, url_for, abort
+from flask_login import current_user
+from ...models.user import Users
+from ...models.tickets import Tickets
+from .Functions.access_validation import agent_acces_val
+
+@app.route('/agent/tickets/<ticket_id>', methods=['GET'])
+@login_required
+def agent_ticket_details(ticket_id):
+    if (current_user.Rol != 'Agente Helpdesk'):
+        return redirect(url_for(agent_acces_val(current_user.Rol)))
+    token = session.get('token')
+    ticket = Tickets.query.get(ticket_id)
+    if ticket is None:
+        abort(404)
+    owner = Users.query.filter_by(id=ticket.User_ID).first()
+    agent = Users.query.filter_by(id=ticket.Agent_ID).first()
+    return render_template('Agente_HelpDesk/ticket_detalle.html', owner=owner, agent=agent, ticket=ticket, token=token, User_session_id=current_user.id)
