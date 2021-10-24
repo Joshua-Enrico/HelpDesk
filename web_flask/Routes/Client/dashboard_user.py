0af521614c3143ccc4c2c0b6722b17204b09d647
rennleon <2829@holbertonschool.com>
2021-10-24 12:20:13 -0500

    Update view and endpoint for user tickets

--- web_flask/Routes/Client/dashboard_user.py
+++ web_flask/Routes/Client/dashboard_user.py
@@ -3,7 +3,7 @@ from ...models import app
 from flask_login import login_required
 from ...models.forms.ticket import TicketForm
 from .Functions.access_validation import client_acces_val
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, session
 from flask_login import current_user
 
 @app.route('/dashboard_user', methods=['GET', 'POST'])
@@ -12,4 +12,5 @@ def dashboard_usuario():
     if (current_user.Rol != 'Usuario'):
         return redirect(url_for(client_acces_val(current_user.Rol)))
     ticket = TicketForm()
-    return render_template('Usuario/dashboard_usuario.html', ticket=ticket, name=current_user.Nombre, User_id=current_user.id)
+    token = session.get('token')
+    return render_template('Usuario/dashboard_usuario.html', ticket=ticket, name=current_user.Nombre, User_id=current_user.id, token=token)
