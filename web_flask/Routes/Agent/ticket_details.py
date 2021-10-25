339e1806d2c3039d0678fc0973a8712238ac9f91
yoyogold-a11 <tmrfack@gmail.com>
2021-10-25 11:58:07 -0500

    fixing bugs

--- web_flask/Routes/Agent/ticket_details.py
+++ web_flask/Routes/Agent/ticket_details.py
@@ -12,6 +12,7 @@ from .Functions.access_validation import agent_acces_val
 @app.route('/agent/tickets/<ticket_id>', methods=['GET'])
 @login_required
 def agent_ticket_details(ticket_id):
+    flag = 0
     if (current_user.Rol != 'Agente Helpdesk'):
         return redirect(url_for(agent_acces_val(current_user.Rol)))
     token = session.get('token')
@@ -21,4 +22,8 @@ def agent_ticket_details(ticket_id):
     owner = Users.query.filter_by(id=ticket.User_ID).first()
     agent = Users.query.filter_by(id=ticket.Agent_ID).first()
     messages = chathistory.query.filter_by(Ticket_id=ticket_id).order_by(asc(chathistory.DateTime))
-    return render_template('Agente_HelpDesk/ticket_detalle.html',messages=messages, owner=owner, agent=agent, ticket=ticket, token=token, User_session_id=current_user.id)
+    try:
+        print(messages[0])
+    except  IndexError:
+        flag = 1
+    return render_template('Agente_HelpDesk/ticket_detalle.html',flag=flag,messages=messages, owner=owner, agent=agent, ticket=ticket, token=token, User_session_id=current_user.id)
--- web_flask/Routes/Client/ticket_details.py
+++ web_flask/Routes/Client/ticket_details.py
@@ -11,6 +11,7 @@ from .Functions.access_validation import client_acces_val
 
 @app.route('/user/tickets/ver/<ticket_id>', methods=['GET'])
 def ver_ticket_user(ticket_id):
+    flag = 0
     if (current_user.Rol != 'Usuario'):
         return redirect(url_for(client_acces_val(current_user.Rol)))
     token = session.get('token')
@@ -18,4 +19,8 @@ def ver_ticket_user(ticket_id):
     owner = Users.query.get(ticket.User_ID)
     agent = Users.query.filter_by(id=ticket.Agent_ID).first()
     messages = chathistory.query.filter_by(Ticket_id=ticket_id).order_by(asc(chathistory.DateTime))
-    return render_template('Usuario/ticket_user.html',messages=messages, owner=owner, agent=agent, ticket=ticket, token=token, User_id=current_user.id)
+    try:
+        print(messages[0])
+    except  IndexError:
+        flag = 1
+    return render_template('Usuario/ticket_user.html',flag=flag, messages=messages, owner=owner, agent=agent, ticket=ticket, token=token, User_id=current_user.id)
