f74d204ff5da729c0303db616aafc3b8319a807a
yoyogold-a11 <tmrfack@gmail.com>
2021-10-26 14:52:47 -0500

    fixing

--- web_flask/Static/scripts/Admin/summary.js
+++ web_flask/Static/scripts/Admin/summary.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
         headers: {'Authorization': 'Bearer ' + $('#token').val()},
         contentType: 'application/json',
         success: data => {
+            result = data[0]
             $("a[name=Complete]").text('Todos tus tickets: ' + result.All_tickets);
             $("a[name=Pending]").text('Tickets Pendientes: ' + result.Pendings);
             $("a[name=Asigned]").text('Todos Asignados: ' + result.Assigned);
